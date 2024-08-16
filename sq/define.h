93a48cdbf3b313f8ba46218bf26811e6b8ef3d2d
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2024-08-16 16:58:39 -0400

    find .sq file

---
+++ sq/define.h
@@ -0,0 +1,18 @@
+//
+//  define.h
+//  sq
+//
+//  Created by Bimal Bhagrath on 2024-08-16.
+//
+
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#define SQ_FILE ".sq"
+#define BUILD_VERSION "1A"
+
+#ifndef BUILD_NUMBER
+#define BUILD_NUMBER 0
+#endif
+
+#endif // DEFINE_H
--- sq/main.m
+++ sq/main.m
@@ -6,16 +6,64 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "define.h"
+#import "SQError.h"
 
-#ifndef BUILD_NUMBER
-#define BUILD_NUMBER 0
-#endif
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"sq build #%d", BUILD_NUMBER);
+int find(char **url) {
+    NSArray *extensions = @[ @SQ_FILE ];
+    NSFileManager *manager = NSFileManager.defaultManager;
+    NSString *path = manager.currentDirectoryPath;
+    NSString *lastPath;
+    NSError *error;
+    BOOL isGitPath = NO;
+    
+    while (!isGitPath && ![path isEqualToString:lastPath]) {
+        NSArray *directory = [manager contentsOfDirectoryAtPath:path error:&error];
+        
+        if (error) return SQPathError;
+        
+        for (NSString *file in directory) {
+            if ([extensions containsObject:file]) {
+                NSString *filePath = [path stringByAppendingPathComponent:file];
+                
+                *url = (char *) malloc((filePath.length + 1) * sizeof(char *));
+                strcpy(*url, filePath.UTF8String);
+                
+                return 0;
+            }
+            
+            isGitPath = isGitPath || [file isEqualToString:@".git"];
+        }
+        
+        lastPath = path;
+        path = [path stringByDeletingLastPathComponent];
     }
+    
+    return 0;
+}
 
+int main(int argc, const char *argv[]) {
+    @autoreleasepool {
+        char *url = NULL;
+        int code = find(&url);
+        
+        if (code) {
+            NSLog(@"(%d)", code);
+            free(url);
+            
+            return code;
+        };
+        
+        NSString *path;
+        
+        if (url) {
+            path = [NSString stringWithCString:url encoding:NSUTF8StringEncoding];
+            free(url);
+        }
+        
+        NSLog(@"sq path %@", path);
+        NSLog(@"sq build %s%d", BUILD_VERSION, BUILD_NUMBER);
+    }
+    
     return 0;
 }
