8654b54b293ae8fc03f9b225974889c25ce57987
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2024-08-16 15:48:36 -0400

    setup macos cli app

---
+++ sq/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sq
+//
+//  Created by Bimal Bhagrath on 2024-08-16.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
