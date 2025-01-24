cd10971a4db3958b755a933719d7327752fe72d1
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2025-01-23 20:53:46 -0500

    setup xcode project

---
+++ sq/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sq
+//
+//  Created by Bimal Bhagrath on 2025-01-23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+    NSLog(@"Hello, SQ!");
+  }
+
+  return 0;
+}
